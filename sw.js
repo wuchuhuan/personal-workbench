ae87c06d8601e55ee867fa988cc850dae6fd4980
wuchuhuan <120439939+wuchuhuan@users.noreply.github.com>
2026-09-18 10:39:43 +0800

    修正离线缓存资源路径

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'personal-workbench-v1';
-const APP_SHELL = ['./', './index.html', './个人工作台.html', './manifest.webmanifest', './assets/app.css', './assets/app.js', './assets/pwa.js', './assets/app-icon.svg'];
+const APP_SHELL = ['./', './index.html', './个人工作台.html', './manifest.webmanifest', './app.css', './app.js', './pwa.js', './app-icon.svg'];
 self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener('fetch', (event) => {
